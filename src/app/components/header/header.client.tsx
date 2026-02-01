b8b822a036d008885bfcd7fdc8ec00fbab7dab21
Iman Baghernejad <iman.baghernejad@gmail.com>
2026-02-01 13:08:59 +0100

    add logo

--- src/app/components/header/header.client.tsx
+++ src/app/components/header/header.client.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { IHeaderClientProps } from "@/types/type";
+import { navigate } from "next/dist/client/components/segment-cache/navigation";
 import Link from "next/link";
 import { useRouter, usePathname } from "next/navigation";
 import { JSX, useEffect, useState } from "react";
@@ -18,6 +19,7 @@ const HeaderClient = ({ pages, socialMediaList, language }: IHeaderClientProps) 
 
     const router = useRouter();
     const pathname = usePathname();
+    
     const segments = pathname.split("/");
     const changeLanguage = (newLang: "en" | "fa" | "de") => {
         const segments = pathname.split("/"); // حذف خالی‌ها
@@ -31,6 +33,10 @@ const HeaderClient = ({ pages, socialMediaList, language }: IHeaderClientProps) 
     useEffect(() => {
         setIsOpen(false)
     },[pathname])
+
+    const goToHome = () => {
+        router.push("/")
+    }
     return (
         <div className="w-full bg-[#222831] sticky top-0 z-50 ">
             <nav className="w-[90%] mb:w-4/5 mx-auto flex px-6 pt-3 text-sm  space-between justify-between ">
@@ -38,7 +44,7 @@ const HeaderClient = ({ pages, socialMediaList, language }: IHeaderClientProps) 
                     <span onClick={() => setIsOpen(true)} className=" capitalize inline-block  md:hidden cursor-pointer text-l hover:text-[#F9C74F]">
                         <FaBars size={30} />
                     </span>
-                    <span><img className="absolute top-1 inset-x-1/5 md:inset-x-1/2  " src={"/logo.jpg"} width={120} height={30}/></span>
+                    <span><img className="absolute top-2 inset-x-1/5 md:inset-x-1/2 cursor-pointer" src={"/logo.png"} onClick={goToHome} width={100} height={30}/></span>
                     <Link href={"/"} style={{ fontFamily: language === "fa" ? "Vazir" : "Roboto" }} className="pt-2 px-5 capitalize hidden md:inline-block cursor-pointer text-l hover:text-[#F9C74F]">{language === "fa" ? "خانه " : "Home"}</Link>
                     {pages.filter(p => p.position === "top").map(p => (
                         <Link key={p.id} href={p.slug ? `/${language}/${p.slug}/${p.id}` : `/${p.url}/${language}/${p.id}`} style={{ fontFamily: language === "fa" ? "Vazir" : "Roboto" }} className="hidden md:inline-block pt-2 px-5 capitalize cursor-pointer text-l hover:text-[#F9C74F]">{p.title}</Link>
@@ -71,7 +77,7 @@ const HeaderClient = ({ pages, socialMediaList, language }: IHeaderClientProps) 
             </nav>
             <div className={`flex flex-col absolute top-0 left-0 h-screen w-80 bg-[#F9C74F] z-10 transition-transform duration-300 ease-in-out ${isOpen? "translate-x-0" : "-translate-x-full"}`}>
                 <div className="flex justify-between px-5 py-5">
-                    <img src={"/logo.jpg"} width={120} height={30}/>
+                    <img src={"/logo.png"} width={120} height={30} onClick={goToHome}/>
                     <div className="text-2xl text-black font-bold" onClick={() => setIsOpen(false)}>X</div>
                 </div>
                 <Link href={"/"} style={{ fontFamily: language === "fa" ? "Vazir" : "Roboto" }} className="pt-2 px-5 font-bold capitalize cursor-pointer text-l text-[#303030] hover:text-[#000000]">{language === "fa" ? "خانه " : "Home"}</Link>
